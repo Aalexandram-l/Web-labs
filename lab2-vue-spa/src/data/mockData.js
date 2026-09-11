2cebad1b2f93215cb98a01b848850bd553556eeb
Alexandra <aleksandracasina475@gmail.com>
2026-09-11 21:15:32 +0600

    Update mockData.js

--- lab2-vue-spa/src/data/mockData.js
+++ lab2-vue-spa/src/data/mockData.js
@@ -1,17 +1,18 @@
 export const prizesData = [
-  { category: 'Физика', date: '2024-12-10', grant: 9.0 },
-  { category: 'Химия', date: '2024-12-10', grant: 9.0 },
-  { category: 'Медицина', date: '2024-12-10', grant: 9.0 },
-  { category: 'Литература', date: '2024-12-10', grant: 9.0 },
-  { category: 'Мира', date: '2024-12-10', grant: 9.0 },
-  { category: 'Экономика', date: '2024-12-10', grant: 9.0 }
+  { category: 'Физика', date: '2025-12-10', grant: 11.0 },
+  { category: 'Химия', date: '2025-12-10', grant: 11.0 },
+  { category: 'Медицина', date: '2025-12-10', grant: 11.0 },
+  { category: 'Литература', date: '2025-12-10', grant: 11.0 },
+  { category: 'Мира', date: '2025-12-10', grant: 11.0 },
+  { category: 'Экономика', date: '2025-12-10', grant: 11.0 }
 ]
 
 export const laureatsData = [
-  { name: 'Альберт Эйнштейн', birth: '1879-03-14', prizes: 1 },
-  { name: 'Мария Кюри', birth: '1867-11-07', prizes: 2 },
-  { name: 'Международный комитет Красного Креста', birth: '1863-02-17', prizes: 3 },
-  { name: 'Джон Бардин', birth: '1908-05-23', prizes: 2 },
-  { name: 'Лайнус Полинг', birth: '1901-02-28', prizes: 2 },
-  { name: 'ООН', birth: '1945-10-24', prizes: 1 }
+  { name: 'Фред Рэмсделл',        birth: '1960-12-04', prizes: 1 },
+  { name: 'Шимон Сакагучи',       birth: '1951-01-19', prizes: 1 },
+  { name: 'Джон Кларк',           birth: '1942-02-10', prizes: 1 },
+  { name: 'Сусуму Китагава',      birth: '1951-07-04', prizes: 1 },
+  { name: 'Ричард Робсон',        birth: '1937-06-04', prizes: 1 },
+  { name: 'Омар Ягхи',            birth: '1965-02-09', prizes: 1 },
+  { name: 'Ласло Краснахоркай',   birth: '1954-01-05', prizes: 1 }
 ]
